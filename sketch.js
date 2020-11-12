82232d0cfef68450fb696060480219ed437e0a36
Vineetha96 <vineethagandepalli@gmail.com>
2020-11-12 20:51:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper,ground,dustbin;
+
+function preaload() {
+
+}
+
+function setup() {
+	createCanvas(1200,700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(200,460,40);
+	dustbin = new Dustbin(1200,100,150,150);
+
+	Engine.run(engine);
+	ground = new Ground(600,height,1200,20);
+  
+}
+
+function keyPressed() {
+	if(keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper.Body,paper.Body.position,{x:30,y:-30});
+	}
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  paper.display();
+  ground.display();
+  dustbin.display();
+}
